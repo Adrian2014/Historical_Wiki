2ad7b3eea8406642fe747709e9122b6e6a88e629
Adrian <modernamun@gmail.com>
2014-08-06 16:31:23 -0500

    User controller, authentication, and login/register

---
+++ Historical_Wiki/app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:id] = @user.id
+      redirect_to users_show_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @posts = Post.where(user_id: session[:id])
+  end
+
+  def login
+    @user = User.new
+    render :login
+  end
+
+
+  def login_post
+    data = params[:data]
+    @user = User.find_by(email: data[:email])
+    auth = user.try(:authenticate, data[:password])
+
+    if auth
+      session[:id] = user.id
+      redirect_to '/'
+    else
+      # flash[:error] = "Invalid email/password. Please try again!"
+      render :login
+    end
+  end
+
+
+
+  def logout
+    session[:id] = nil
+    redirect_to '/'
+  end
+
+  private
+  def user_params
+    params.require(:post).permit(:username, :email, :password, :password_confirmation)
+  end
+
+end
