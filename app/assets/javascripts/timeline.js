2523ca7f6e8d7cbf095726bc2d54aa6e5e650660
Spencer Zhang <syz4@cornell.edu>
2014-08-07 16:11:09 -0500

    Implemented timeline

--- app/assets/javascripts/timeline.js
+++ app/assets/javascripts/timeline.js
@@ -2,11 +2,6 @@ var startYear=1900, endYear=2000, precision=10;
 var animating = false;
 var blocks = [];
 
-var Block = function(posts, index) {
-  this.posts = posts;
-  this.i = index;
-}
-
 window.onload = function() {
   var data, timeline
 
@@ -17,22 +12,77 @@ window.onload = function() {
   timeline.append("div").attr('class', 'canvas');
   timeline.append("span").attr('class', 'start-date').text(startYear);
   timeline.append("span").attr('class', 'end-date').text(endYear);
+  timeline.append("div").attr('class', 'dropdown')
   d3.select('body').append("div").attr('class', 'clear')
 
+  hideDropdown();
+
   // Create the blocks on the timeline
-  getData(startYear, endYear, precision);
+  getData();
 
   // add onhover methods for the class
+  $('.timeline').on('mouseenter', 'svg', function() {
+    showDropdown(this);
+  })
 
+  $('.timeline').on('click', 'svg', function() {
+    if(precision > 1) {
+      precision = precision / 10
+      getData();
+    }
+  })
+
+  $('.timeline').on('mouseleave', function() {
+    hideDropdown();
+  })
 }
 
-function getData(startYear, endYear, precision) {
+function showDropdown(element) {
+  offset = $(element).css('left');
+  $('.timeline .dropdown').html($(element).find('div').html());;
+  $('.timeline .dropdown').css('left', offset);
+  $('.timeline .dropdown').show();
+}
+
+function hideDropdown() {
+  $('.timeline .dropdown').hide();
+}
+
+function getData() {
   var data;
   $.get(
     '/posts/getdata', 
     { start_year:startYear, end_year:endYear, precision:precision },
     function(response) {
-      blocks = response
+      blocks = response;
+      updateCanvas();
     }, 'json'
   )
+}
+
+function updateCanvas() {
+  var baseIndex = Math.floor(startYear/precision);
+  var index = 0
+  var nBlocks = Math.floor((endYear - startYear) / precision);
+  var canvasWidth = $('.timeline .canvas').width();
+  var blockWidth = canvasWidth / nBlocks
+  var block, div, blockData
+
+  $('.timeline .canvas').text('');
+
+  while((baseIndex + index)*precision < endYear) {
+    if(blocks[index+baseIndex]){
+      block = d3.select('.timeline .canvas').append("svg")
+        .classed("block", true)
+        .style("width", blockWidth)
+        .style("left", index*blockWidth);
+      div = block.append("div");
+      blockData = blocks[index+baseIndex]
+      for(post in blockData) {
+        div.append("li")
+          .text(blockData[post].post_title)
+      };
+    }
+    index += 1;
+  }
 }
