fcb914f54d9dc031fe50938e3219f1e8fe8d7b65
Spencer Zhang <syz4@cornell.edu>
2014-08-07 12:47:22 -0500

    Added methods to Post model

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,6 @@ module PostsHelper
   def current_user   #MAKE SURE TO SET session[:current] = CURRENT USER ID!!! DONE IN CONTROL
     @current_user ||= User.find_by(id: session[:id])
   end
+
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,28 @@
 #
 
 class Post < ActiveRecord::Base
+    def self.between_years(start_year, end_year)
+      start_date = "#{start_year}-01-01"
+      end_date = "#{end_year-1}-12-31"
+      Post.where(post_date: start_date..end_date).order(:post_date)
+    end
+
+    def self.get_data(start_year, end_year, precision)
+      posts = Post.between_years(start_year, end_year).to_a
+      data = Hash.new
+      block = start_year/precision
+
+      posts.each do |post|
+        while post.year >= (block+1)*precision
+          block += 1
+        end
+        data[block] = [] unless data[block]
+        data[block].push post
+      end
+
+      return data
+    end
+
     belongs_to :user
     has_many :post_tags
     has_many :tags, through: :post_tags
@@ -18,5 +40,9 @@ class Post < ActiveRecord::Base
 
     def date
       @date ||= Date.parse(self.post_date)
+    end
+
+    def year
+      self.date.year
     end
 end
