3500d1abe8f844720a129baeb6335f912454c9ac
Spencer Zhang <syz4@cornell.edu>
2014-08-10 18:35:42 -0500

    Changed controller action after destroying post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -93,7 +93,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to '/users/show', notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
