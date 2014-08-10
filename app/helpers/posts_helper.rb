0978345523b62dde9c9a0562ac946f7825f19c60
Steve Loar <sjl7909@gmail.com>
2014-08-10 10:55:48 -0500

    Pulling down changes to look

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,6 +2,9 @@ module PostsHelper
   def current_user   #MAKE SURE TO SET session[:current] = CURRENT USER ID!!! DONE IN CONTROL
     @current_user ||= User.find_by(id: session[:id])
   end
-
+  
+  def last_five
+  	@posts = Post.all.sort_by(&:updated_at).last(5).reverse
+  end
   
 end
